fe55dfdd63b71d934d3d3e77a07c7a121a3c16a0
Patrick_Holyoake <patrick.holyoake@outlook.com>
2021-06-05 21:42:32 +1000

    completed basic version

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -10,7 +10,7 @@ function makeResponsive() {
     }
     
     // Svg paramaters
-    var svgWidth = window.innerWidth - 100;
+    var svgWidth = window.innerWidth - 1000;
     var svgHeight = window.innerHeight - 400;
 
     var margin = {
@@ -38,8 +38,86 @@ function makeResponsive() {
         
         // Preview the data
         console.log(censusData);
+
+        censusData.forEach(data => {
+            data.healthcare = +data.healthcare;
+            data.poverty = +data.poverty;
+        });
+
+        //Xscale
+        var xScale = d3.scaleLinear()
+            .domain([d3.min(censusData, d => d.poverty)-1, d3.max(censusData, d => d.poverty)+2])
+            .range([0, width]);
+
+        // yScale
+        var yScale = d3.scaleLinear()
+            .domain([d3.min(censusData, d => d.healthcare)-0.5, d3.max(censusData, d => d.healthcare)+2])
+            .range([height, 0]);
+
+        // Build Axes
+        var bottomAxis = d3.axisBottom(xScale);
+        var leftAxis = d3.axisLeft(yScale);
+
+        chartGroup.append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(bottomAxis);
+
+        chartGroup.append("g")
+            .call(leftAxis);
+
+        // Circles
+        var circlesGroup = chartGroup.attr("class", "nodes")
+            .selectAll("circle")
+            .data(censusData)
+            .enter()
+            .append("g");
+
+            circlesGroup.append("circle")
+            .attr("class", "node")
+            .attr("r", 10)
+            .classed("stateCircle", true)
+            .attr("cx", d => xScale(d.poverty))
+            .attr("cy", d => yScale(d.healthcare));
+    
+        circlesGroup.append("text")
+            .classed("stateText", true)
+            .attr("x", d => xScale(d.poverty))
+            .attr("y", d => yScale(d.healthcare))
+            .attr("alignment-baseline", "central")
+            .style("font-size", "10px")
+            .text(d => d.abbr);
+
+        // X axis labels
+        var labelsGroup = chartGroup.append("g")
+            .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+        labelsGroup.append("text")
+            .attr("x", 0)
+            .attr("y", 20)
+            .classed("aText", true)
+            .text("In Poverty (%)");
         
-    }
+        // append y axis
+        chartGroup.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - margin.left)
+            .attr("x", 0 - (height / 2))
+            .attr("dy", "1em")
+            .classed("aText", true)
+            .text("Lacks Healthcare (%)");
+        
+    })
+
+}
+
+makeResponsive();
+
+d3.select(window).on("resize", makeResponsive);
+
+
+
+        
+    
 
 
 
